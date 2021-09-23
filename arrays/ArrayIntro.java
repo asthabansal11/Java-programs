72d5adce02de34937e3903605017b1700ff02bdf
asthabansal11 <67437836+asthabansal11@users.noreply.github.com>
2021-09-23 11:16:12 +0530

    Add files via upload

---
+++ arrays/ArrayIntro.java
@@ -0,0 +1,24 @@
+public class ArrayIntro {
+    public static void main(String args[]) {
+
+        // int[] marks = new int[5];
+
+        // int[] marks;
+        // marks = new int[10];
+
+        // int marks[] = new int[5];
+
+        // System.out.println(marks[5]);
+        // int rollno[] , classes[];
+
+        int[] age = { 2, 3, 54, 64, 3 };
+        // double[] marks = {1.3, 4.4, 3.4};
+        // marks[2] = 32.4;
+        // System.out.println(marks[2]);
+
+        for (int i = 0; i < age.length; i++) {
+            System.out.println(age[i]);
+        }
+
+    }
+}
